2d8d45fe6c4add34ef5d95e147be9878309ba450
SumeetRohilla <sumeetrohilla@gmail.com>
2021-01-12 15:31:56 +0100

    Update readPTU_FLIM.py
    Update for sync overflow correction for MultiHarp (see Line 519) during long measurement.
    Thanks to r.molenaar@utwente.nl for the fix

--- readPTU_FLIM.py
+++ readPTU_FLIM.py
@@ -516,7 +516,7 @@ class PTUreader():
 
         # Fill in the correct sync values for overflow location    
         #sync[np.where(index == 1)] = 1 # assert values of sync = 1 just right after overflow to avoid any overflow-correction instability in next step
-        if record_type in ['rtHydraHarp2T3','rtTimeHarp260PT3']:
+        if record_type in ['rtHydraHarp2T3','rtTimeHarp260PT3',,'rtMultiHarpNT3']:
             sync    = sync + (WRAPAROUND*np.cumsum(index*sync)) # For HydraHarp V1 and TimeHarp260 V1 overflow corrections 
         else:
             sync    = sync + (WRAPAROUND*np.cumsum(index)) # correction for overflow to sync varibale
